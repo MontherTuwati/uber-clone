56ed39d69903608030500798d9921cce8c031f89
Monther Tuwati <tuwa0001@algonquinlive.com>
2024-09-19 18:25:51 -0400

    Setup and Implemented TailwindCSS

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
+import { NativeWindStyleSheet } from "nativewind";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber Clone!</Text>
+      <View>
+        <HomeScreen/>
       </View>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text className='text-red-500 p-10'>HomeScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
