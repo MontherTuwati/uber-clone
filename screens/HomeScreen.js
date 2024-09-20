0df0ec8ba85a7e938cc7731083f35af6a8d64130
Monther Tuwati <tuwa0001@algonquinlive.com>
2024-09-20 01:50:42 -0400

    Built HomeScreen Components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { Image, SafeAreaView, View } from 'react-native'
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
-            <Text className='text-red-500 p-10'>HomeScreen</Text>
+        <SafeAreaView className='bg-white h-full'>
+            <View className='p-5'>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode:'contain',
+                    }}
+                    source={require('../assets/uber_logo.png')}
+                />
+            </View>
         </SafeAreaView>
     )
 }
