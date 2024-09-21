e2ad54f9967aeec37cdf5982d544d9bea2584b68
Monther Tuwati <tuwa0001@algonquinlive.com>
2024-09-21 01:27:39 -0400

    Built NavOptions and Implemented Heroicons

---
+++ components/NavOptions.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
+
+const data = [
+    {
+        id: '123',
+        title: 'Rides',
+        image: require('../assets/UberX.png'),
+        screen: 'MapScreen',
+    },
+    {
+        id: '456',
+        title: 'Eats',
+        image: require('../assets/FoodBowl.png'),
+        screen: 'EatsScreen',
+    },
+]
+
+const NavOptions = () => {
+    return (
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({item}) => (
+                <TouchableOpacity>
+                    <View className='flex-row items-center'>
+                        <Image
+                            style={{
+                                width: 50,
+                                height: 50,
+                                resizeMode: 'contain'
+                            }}
+                            source={item.image}
+                        />
+                        <Text className='font-bold text-xl'>{item.title}</Text>
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    )
+}
+
+export default NavOptions
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { Image, SafeAreaView, View } from 'react-native'
+import { SafeAreaView, Text, TextInput, View } from 'react-native'
+import { ChevronDownIcon } from 'react-native-heroicons/mini'
+import { ClockIcon, MagnifyingGlassIcon } from 'react-native-heroicons/solid'
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView className='bg-white h-full'>
-            <View className='p-5'>
-                <Image
-                    style={{
-                        width: 100,
-                        height: 100,
-                        resizeMode:'contain',
-                    }}
-                    source={require('../assets/uber_logo.png')}
-                />
+            <View className='p-5 pt-16'>
+                <NavOptions/>
+            </View>
+            {/*Search Bar*/}
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row rounded-full border items-center space-x-3 flex-1 bg-gray-200 border-gray-200 p-2">
+                    <MagnifyingGlassIcon color='black' size={30} stroke={250}/>
+                    <TextInput
+                        placeholder="Where to?"
+                        keyboardType='default'
+                        className='ml-2 flex-1 text-xl font-extrabold'
+                    />
+                    <View className='relative flex-row'>
+                        <View className='flex-row items-center space-x-2 border-0 border-l pl-4 border-white'>
+                            <View className='flex-row rounded-full bg-white items-center p-2 space-x-3'>
+                                <ClockIcon size={25} color='black' stroke={0}/>
+                                <Text className='text-black font-medium'>Now</Text>
+                                <ChevronDownIcon color='black' size={25} stroke={200}/>
+                            </View>
+                        </View>
+                    </View>
+                    
+                </View>
             </View>
         </SafeAreaView>
     )
